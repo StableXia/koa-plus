0cf19906124ec542a6b9473b3770cf64c5b290e4
Wen Xia <wen.xia@wework.com>
2020-11-30 14:24:21 +0800

    docs: e

--- example/server.js
+++ example/server.js
@@ -1,9 +1,33 @@
-const KoaPlus = require("../packages/koa/lib");
-const Router = require("../packages/router/lib");
-const static = require("../packages/static/lib");
+const KoaPlus = require("../packages/koa");
+const Router = require("../packages/router");
+const static = require("../packages/static");
 
 const server = new KoaPlus();
 const router = new Router();
+
+function delay(s) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, s * 1000);
+  });
+}
+
+server.use(async (ctx, next) => {
+  await next();
+});
+
+server.use(async (ctx, next) => {
+  const { url } = ctx;
+  console.log("end 123");
+  if (url === "/") {
+    await delay(10);
+    ctx.body = "123";
+    next();
+  } else {
+    next();
+  }
+});
 
 server.use(static(__dirname + "/public"));
 
