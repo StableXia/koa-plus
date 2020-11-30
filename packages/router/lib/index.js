548a254db2a15561ec737bfe6006f055a6aa8e7f
Wen Xia <wen.xia@wework.com>
2020-11-30 11:27:15 +0800

    build: lerna

---
+++ packages/router/lib/index.js
@@ -0,0 +1,44 @@
+class Router {
+  constructor() {
+    this.stack = [];
+  }
+
+  register(path, method, middleware) {
+    this.stack.push({ path, method, middleware });
+  }
+
+  get(path, middleware) {
+    this.register(path, "get", middleware);
+  }
+
+  post(path, middleware) {
+    this.register(path, "post", middleware);
+  }
+
+  routes() {
+    const stack = this.stack;
+
+    return function (ctx, next) {
+      let route;
+
+      for (let i = 0; i < stack.length; i++) {
+        let item = stack[i];
+
+        if (item.path === ctx.url && item.method === ctx.method) {
+          // 判断path和method
+          route = item.middleware;
+          break;
+        }
+      }
+
+      if (typeof route === "function") {
+        route(ctx, next);
+        return;
+      }
+
+      next();
+    };
+  }
+}
+
+module.exports = Router;
